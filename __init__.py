18a0a76e1e8475fd74ce4d8ee6d0b588b648e62a
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-02 13:55:00 -0500

    adding init file for repo

---
+++ __init__.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_pymongo import PyMongo
+from flask import make_response
+from bson.json_util import dumps
+
+MONGO_URL = os.environ.get('MONGO_URL')
+if not MONGO_URL:
+    MONGO_URL = "mongodb://localhost:27017/run"
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = MONGO_URL
+mongo = PyMongo(app)
+
+
+def output_json(obj, code, headers=None):
+    resp = make_response(dumps(obj), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+
+DEFAULT_REPRESENTATIONS = {'application/json': output_json}
+api = Api(app)
+api.representations = DEFAULT_REPRESENTATIONS
+
+
+class Root(Resource):
+    def get(self):
+        return {
+            'status': 'OK',
+            'mongo': str(mongo.db),
+        }
+
+
+api.add_resource(Root, '/')
+
+
+from xenon_runsDB_api.runs import *
+from xenon_runsDB_api.run import *
