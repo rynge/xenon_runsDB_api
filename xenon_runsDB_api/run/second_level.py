e5cfa4c300766f29a0540b954f366d5556d0839b
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-15 18:24:58 -0500

    Getting things to work

--- xenon_runsDB_api/run/second_level.py
+++ xenon_runsDB_api/run/second_level.py
@@ -1,43 +1,58 @@
+from flask import abort
 from flask_restful import Resource
+from xenon_runsDB_api.util import get_result_ready
 from xenon_runsDB_api.app import app, api, mongo
 
-
-def get_result_ready(result, top_level, second_level):
-    if (isinstance(result[top_level], list) or
-        isinstance(result[top_level], tuple)):
-        filtered_results = [record[second_level]
-                            for record in result[top_level]]
-        return filtered_results
-    if isinstance(result[top_level], dict):
-        return result[top_level][second_level]
-    if not (isinstance(result[top_level], list) or
-            isinstance(result[top_level], tuple) or
-            isinstance(result[top_level], dict)):
-        return 404
-
+"""
+Getting the entire document for a run given a unique identifier for
+the desired run. In this case there are three possible identifiers
+the database specific documennt or object ID, the run number
+(or run id), and the timestamp in YYMMDD_HHMM format.
+"""
 
 class RunSecondLevelObjectID(Resource):
     def get(self, object_id, top_level, second_level):
         app.logger.debug("Requesting data for run with object ID %s"
                          % object_id)
-        result = mongo.db.runs_new.find_one_or_404({"_id": object_id})
-        return get_result_ready(result, top_level, second_level)
+        result = mongo.db.runs_new.find_one_or_404(
+            {"_id": object_id},
+            {"_id": 0,
+             "%s.%s" % (top_level, second_level): 1})
+        app.logger.debug("Query Result %s" % result)
+        if not result:
+            return abort(404)
+        else:
+            return get_result_ready(result, top_level, second_level)
 
 
 class RunSecondLevelRunID(Resource):
     def get(self, run_id, top_level, second_level):
         app.logger.debug("Requesting data for run with run number %s"
                          % run_id)
-        result = mongo.db.runs_new.find_one_or_404({"number": run_id})
-        return get_result_ready(result, top_level, second_level)
+        result = mongo.db.runs_new.find_one_or_404(
+            {"number": run_id},
+            {"_id": 0,
+             "%s.%s" % (top_level, second_level): 1})
+        app.logger.debug("Query Result %s" % result)
+        if not result:
+            return abort(404)
+        else:
+            return get_result_ready(result, top_level, second_level)
 
 
 class RunSecondLevelTimestamp(Resource):
     def get(self, timestamp, top_level, second_level):
         app.logger.debug("Requesting data for run with timestamp %s"
                          % timestamp)
-        result = mongo.db.runs_new.find_one_or_404({"name": timestamp})
-        return get_result_ready(result, top_level, second_level)
+        result = mongo.db.runs_new.find_one_or_404(
+            {"name": timestamp},
+            {"_id": 0,
+             "%s.%s" % (top_level, second_level): 1})
+        app.logger.debug("Query Result %s" % result)
+        if not result:
+            return abort(404)
+        else:
+            return get_result_ready(result, top_level, second_level)
 
 
 api.add_resource(RunSecondLevelObjectID,
