d77175bf2c6e721ad0464830c7177153e8f9a485
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-15 18:10:28 -0500

    Removing extra code

--- __init__.py
+++ __init__.py
@@ -1,41 +1,41 @@
-import os
-from flask import Flask
-from flask_restful import Resource, Api
-from flask_pymongo import PyMongo
-from flask import make_response
-from bson.json_util import dumps
+# import os
+# from flask import Flask
+# from flask_restful import Resource, Api
+# from flask_pymongo import PyMongo
+# from flask import make_response
+# from bson.json_util import dumps
 
-MONGO_URL = os.environ.get('MONGO_URL')
-if not MONGO_URL:
-    MONGO_URL = "mongodb://localhost:27017/run"
+# MONGO_URL = os.environ.get('MONGO_URL')
+# if not MONGO_URL:
+#     MONGO_URL = "mongodb://localhost:27017/run"
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-app.config['MONGO_URI'] = MONGO_URL
-mongo = PyMongo(app)
-
-
-def output_json(obj, code, headers=None):
-    resp = make_response(dumps(obj), code)
-    resp.headers.extend(headers or {})
-    return resp
+# app.config['MONGO_URI'] = MONGO_URL
+# mongo = PyMongo(app)
 
 
-DEFAULT_REPRESENTATIONS = {'application/json': output_json}
-api = Api(app)
-api.representations = DEFAULT_REPRESENTATIONS
+# def output_json(obj, code, headers=None):
+#     resp = make_response(dumps(obj), code)
+#     resp.headers.extend(headers or {})
+#     return resp
 
 
-class Root(Resource):
-    def get(self):
-        return {
-            'status': 'OK',
-            'mongo': str(mongo.db),
-        }
+# DEFAULT_REPRESENTATIONS = {'application/json': output_json}
+# api = Api(app)
+# api.representations = DEFAULT_REPRESENTATIONS
 
 
-api.add_resource(Root, '/')
+# class Root(Resource):
+#     def get(self):
+#         return {
+#             'status': 'OK',
+#             'mongo': str(mongo.db),
+#         }
 
 
-from xenon_runsDB_api.runs import *
-from xenon_runsDB_api.run import *
+# api.add_resource(Root, '/')
+
+
+# from xenon_runsDB_api.runs import *
+# from xenon_runsDB_api.run import *
