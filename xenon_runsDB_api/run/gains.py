15bd3c79999dd5c5d0de14e9f98bea7189e2d265
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-15 18:21:23 -0500

    Adding ability to change gains through put

--- xenon_runsDB_api/run/gains.py
+++ xenon_runsDB_api/run/gains.py
@@ -1,41 +1,77 @@
+import flask
+from webargs import fields
+from webargs.flaskparser import use_kwargs
 from flask_restful import Resource
 from xenon_runsDB_api.app import app, api, mongo
 
 
-def get_result_ready(result):
-        return result["processor"]["DEFAULT"]["gains"]
+gain_args = {"gains": 
+            fields.List(fields.Float(validate=lambda val: val >= 0.),
+                                     required=True)}
 
 
-class RunGainsObjectID(Resource):
-    def get(self, object_id):
-        app.logger.debug("Requesting data for run with object ID %s"
-                         % object_id)
-        result = mongo.db.runs_new.find_one_or_404({"_id": object_id})
-        return get_result_ready(result)
+class Gains(Resource):
+    def _put_(self, key, value, gains):
+        run_doc_before = mongo.db["runs_new"].find_one_or_404(
+            {key: value})
+        mongo.db["runs_new"].find_one_and_update(
+            {key: value},
+            {"$set": {"processor.DEFAULT.gains": gains}}
+        )
+        run_doc_after = mongo.db["runs_new"].find_one_or_404(
+            {key: value})
+        return flask.jsonify({"previous_run_doc": run_doc_before,
+                              "new_run_doc": run_doc_after})
 
 
-class RunGainsRunID(Resource):
-    def get(self, run_id):
-        app.logger.debug("Requesting data for run with run number %s"
-                         % run_id)
-        result = mongo.db.runs_new.find_one_or_404({"number": run_id})
-        return get_result_ready(result)
+class RunObjectIDGains(Gains):
+    @use_kwargs(gain_args, locations=["json"])
+    def put(self, object_id, gains):
+        return self._put_("_id", object_id, gains)
 
 
-class RunGainsTimestamp(Resource):
-    def get(self, timestamp):
-        app.logger.debug("Requesting data for run with timestamp %s"
-                         % timestamp)
-        result = mongo.db.runs_new.find_one_or_404({"name": timestamp})
-        return get_result_ready(result)
+class RunRunIDGains(Gains):
+    @use_kwargs(gain_args, locations=["json"])
+    def put(self, run_number, gains):
+        return self._put_("number", run_number, gains)
 
 
-api.add_resource(RunGainsObjectID,
-                 ('/run/objectid/<ObjectId:object_id>/'
-                  'processor/DEFAULT/gains/'))
-api.add_resource(RunGainsRunID,
-                 ('/run/runnumber/<int:run_id>/'
-                  'processor/DEFAULT/gains/'))
-api.add_resource(RunGainsTimestamp,
-                 ('/run/timestamp/<string:timestamp>/'
-                  'processor/DEFAULT/gains/'))
+class RunTimestampGains(Gains):
+    @use_kwargs(gain_args, locations=["json"])
+    def put(self, timestamp, gains):
+        return self._put_("name", timestamp, gains)
+
+
+# Adding routes according what identifier is used.
+api.add_resource(RunObjectIDGains,
+                 '/run/objectid/<ObjectId:object_id>/gains',
+                 endpoint="run_object_id_gains")
+api.add_resource(RunRunIDGains,
+                 '/run/runnumber/<int:run_number>/gains',
+                 endpoint="run_run_id_gains")
+api.add_resource(RunRunIDGains,
+                 '/run/number/<int:run_number>/gains',
+                 endpoint="run_run_number_gains")
+api.add_resource(RunTimestampGains,
+                 '/run/timestamp/<string:timestamp>/gains',
+                 endpoint="run_timestamp_gains")
+api.add_resource(RunTimestampGains,
+                 '/run/name/<string:timestamp>/gains',
+                 endpoint="run_timestamp_name_gains")
+# Including the path for runs. Just in case we change our
+# mind down the road
+api.add_resource(RunObjectIDGains,
+                 '/runs/objectid/<ObjectId:object_id>/gains',
+                 endpoint="runs_object_id_gains")
+api.add_resource(RunRunIDGains,
+                 '/runs/runnumber/<int:run_number>/gains',
+                 endpoint="runs_run_id_gains")
+api.add_resource(RunRunIDGains,
+                 '/runs/number/<int:run_number>/gains',
+                 endpoint="runs_run_number_gains")
+api.add_resource(RunTimestampGains,
+                 '/runs/timestamp/<string:timestamp>/gains',
+                 endpoint="runs_timestamp_gains")
+api.add_resource(RunTimestampGains,
+                 '/runs/name/<string:timestamp>/gains',
+                 endpoint="runs_timestamp_name_gains")
