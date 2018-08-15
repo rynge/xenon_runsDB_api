5b80ed79d3ea6a543ff8d1aece956a9a4ea020c3
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-15 18:25:29 -0500

    Getting things to work for third level

--- xenon_runsDB_api/run/third_level.py
+++ xenon_runsDB_api/run/third_level.py
@@ -1,62 +1,55 @@
+from flask import abort
 from flask_restful import Resource
 from xenon_runsDB_api.app import app, api, mongo
 
+"""
+Getting the entire document for a run given a unique identifier for
+the desired run. In this case there are three possible identifiers
+the database specific documennt or object ID, the run number
+(or run id), and the timestamp in YYMMDD_HHMM format.
+"""
 
 class RunThirdLevelObjectID(Resource):
     def get(self, object_id, top_level, second_level, third_level):
         app.logger.debug("Requesting data for run with object ID %s"
                          % object_id)
-        result = mongo.db.runs_new.find_one_or_404({"_id": object_id})
-        return 404
-        # if (isinstance(result[top_level], list) or
-        #     isinstance(result[top_level], tuple)):
-        #     filtered_results = [record[second_level]
-        #                         for record in result[top_level]]
-        #     return filtered_results
-        # if isinstance(result[top_level], dict):
-        #     return result[top_level][second_level]
-        # if not (isinstance(result[top_level], list) or
-        #         isinstance(result[top_level], tuple) or
-        #         isinstance(result[top_level], dict)):
-        #     return 404
+        result = mongo.db.runs_new.find_one_or_404(
+            {"_id": object_id},
+            {"_id": 0,
+             "%s.%s.%s" % (top_level, second_level, third_level): 1})
+        app.logger.debug("Query Result %s" % result)
+        if not result:
+            return abort(404)
+        else:
+            return result[top_level][second_level][third_level]
 
 
 class RunThirdLevelRunID(Resource):
     def get(self, run_id, top_level, second_level, third_level):
         app.logger.debug("Requesting data for run with run number %s"
                          % run_id)
-        result = mongo.db.runs_new.find_one_or_404({"number": run_id})
-        return 404
-        # if (isinstance(result[top_level], list) or
-        #     isinstance(result[top_level], tuple)):
-        #     filtered_results = [record[second_level]
-        #                         for record in result[top_level]]
-        #     return filtered_results
-        # if isinstance(result[top_level], dict):
-        #     return result[top_level][second_level]
-        # if not (isinstance(result[top_level], list) or
-        #         isinstance(result[top_level], tuple) or
-        #         isinstance(result[top_level], dict)):
-        #     return 404
+        result = mongo.db.runs_new.find_one_or_404(
+            {"number": run_id},
+            {"_id": 0,
+             "%s.%s.%s" % (top_level, second_level, third_level): 1})
+        if not result:
+            return abort(404)
+        else:
+            return result[top_level][second_level][third_level]
 
 
 class RunThirdLevelTimestamp(Resource):
     def get(self, timestamp, top_level, second_level, third_level):
         app.logger.debug("Requesting data for run with timestamp %s"
                          % timestamp)
-        result = mongo.db.runs_new.find_one_or_404({"name": timestamp})
-        return 404
-        # if (isinstance(result[top_level], list) or
-        #     isinstance(result[top_level], tuple)):
-        #     filtered_results = [record[second_level]
-        #                         for record in result[top_level]]
-        #     return filtered_results
-        # if isinstance(result[top_level], dict):
-        #     return result[top_level][second_level]
-        # if not (isinstance(result[top_level], list) or
-        #         isinstance(result[top_level], tuple) or
-        #         isinstance(result[top_level], dict)):
-        #     return 404
+        result = mongo.db.runs_new.find_one_or_404(
+            {"name": timestamp},
+            {"_id": 0,
+             "%s.%s.%s" % (top_level, second_level, third_level): 1})
+        if not result:
+            return abort(404)
+        else:
+            return result[top_level][second_level][third_level]
 
 
 api.add_resource(RunThirdLevelObjectID,
