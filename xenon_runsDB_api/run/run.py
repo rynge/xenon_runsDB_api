d8211d4b088a1b21e1b7a49a8cb898bb5e2e4e8b
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-15 18:23:36 -0500

    generalizing code more. adding endpoints at /runs as well as /run

--- xenon_runsDB_api/run/run.py
+++ xenon_runsDB_api/run/run.py
@@ -1,67 +1,82 @@
+import flask
 from flask_restful import Resource
 from xenon_runsDB_api.app import app, api, mongo
 
+"""
+Deleting, getting, insertingm updating, the entire document for 
+a run given a unique identifier for the desired run. In this 
+case there are three possible identifiers the database specific
+documennt or object ID, the run number(or run id), 
+and the timestamp in YYMMDD_HHMM format.
+"""
 
-class RunObjectID(Resource):
-    def get(self, object_id):
-        app.logger.debug("Requesting data for run with object ID %s"
-                         % object_id)
-        result = mongo.db.runs_new.find_one_or_404({"_id": object_id})
+
+class Run(Resource):
+    def _get_(self, key, value):
+        app.logger.debug("Requesting data for run with %s %s"
+                         % (key, value))
+        result = mongo.db["runs_new"].find_one_or_404(
+            {key: value})
         app.logger.debug("Keys for result: %s" % result.keys())
-        return result
+        return flask.jsonify({"results": result})
+    def _delete_(self, key, value):
+        mongo.db["runs_new"].delete_one({key: value})
+        return '', 204
+
+class RunObjectID(Run):
+    def get(self, object_id):
+        return self._get_("_id", object_id)
 
     def delete(self, object_id):
-        app.logger.debug("Deleting data for run with object ID %s"
-                         % object_id)
-        mongo.db.runs_new.find_one_or_404({"_id": object_id})
-        mongo.db.runs_new.remove({"_id": object_id})
-        return '', 204
+        return self._delete_("_id", object_id)
 
 
-class RunRunID(Resource):
-    def get(self, run_id):
-        app.logger.debug("Requesting data for run with run number %s"
-                         % run_id)
-        result = mongo.db.runs_new.find_one_or_404({"number": run_id})
-        app.logger.debug("Keys for result: %s" % result.keys())
-        return result
+class RunRunNumber(Run):
+    def get(self, run_number):
+        return self._get_("number", run_number)
 
-    def delete(self, run_id):
-        mongo.db.runs_new.find_one_or_404({"number": run_id})
-        mongo.db.runs_new.remove({"number": run_id})
-        return '', 204
+    def delete(self, run_number):
+        return self._delete_("number", run_number)
 
 
-class RunTimestamp(Resource):
+class RunTimestamp(Run):
     def get(self, timestamp):
-        app.logger.debug("Requesting data for run with timestamp %s"
-                         % timestamp)
-        result = mongo.db.runs_new.find_one_or_404({"name": timestamp})
-        app.logger.debug("Keys for result: %s" % result.keys())
-        return result
+        return self._get_("name", timestamp)
 
     def delete(self, timestamp):
-        mongo.db.runs_new.find_one_or_404({"name": timestamp})
-        mongo.db.runs_new.remove({"name": timestamp})
-        return '', 204
+        return self._delete_("name", timestamp)
 
 
+# Adding routes according what identifier is used.
 api.add_resource(RunObjectID,
                  '/run/objectid/<ObjectId:object_id>/',
                  endpoint="run_object_id")
-api.add_resource(RunRunID,
-                 '/run/runnumber/<int:run_id>/',
-                 endpoint="run_run_id")
+api.add_resource(RunRunNumber,
+                 '/run/runnumber/<int:run_number>/',
+                 endpoint="run_run_number")
+api.add_resource(RunRunNumber,
+                 '/run/number/<int:run_number>/',
+                 endpoint="run_number")
 api.add_resource(RunTimestamp,
                  '/run/timestamp/<string:timestamp>/',
                  endpoint="run_timestamp")
-
+api.add_resource(RunTimestamp,
+                 '/runs/name/<string:timestamp>/',
+                 endpoint="run_timestamp_name")
+# Including the path for runs. Just in case we change our
+# mind down the road
 api.add_resource(RunObjectID,
                  '/runs/objectid/<ObjectId:object_id>/',
                  endpoint="runs_object_id")
-api.add_resource(RunRunID,
-                 '/runs/runnumber/<int:run_id>/',
+api.add_resource(RunRunNumber,
+                 '/runs/runnumber/<int:run_number>/',
                  endpoint="runs_run_id")
+api.add_resource(RunRunNumber,
+                 '/runs/number/<int:run_number>/',
+                 endpoint="runs_run_number")
 api.add_resource(RunTimestamp,
                  '/runs/timestamp/<string:timestamp>/',
                  endpoint="runs_timestamp")
+api.add_resource(RunTimestamp,
+                 '/runs/name/<string:timestamp>/',
+                 endpoint="runs_timestamp_name")
