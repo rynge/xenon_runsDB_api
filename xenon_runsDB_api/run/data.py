165c32e016d61726427bf71b024704326fa32ac3
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-16 13:19:30 -0500

    checkpoint data.py

--- xenon_runsDB_api/run/data.py
+++ xenon_runsDB_api/run/data.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource
+from webargs import fields
 from xenon_runsDB_api import util
 from xenon_runsDB_api.app import app, api
 
@@ -27,78 +28,91 @@ data_raw_args = {
 
 
 class RunData(Resource):
-    def _get_(self, key, value):
+    def _get_(self, key, value, data_type=None):
         app.logger.debug("Requesting data for run with %s %s"
                          % (key, value))
         result = mongo.db["runs_new"].find_one_or_404(
             {key: value}, 
-            {"data": 1, "name": 1, "_id": 1, "number": 1})
+            {"data": 1, "name": 1, "_id": 1, "number": 1}
+        )
+        print(results)
         app.logger.debug("Keys for result: %s" % result.keys())
-        return flask.jsonify({"results": result})
+        return result
     
-    def _put_normal_data_(self, key, value, data):
-
-    @use_kwargs(data_raw_args, locations=["json"])
-    def _put_raw_data_(self):
+    def _post_normal_data_(self, key, value, data):
+        run_doc_before = mongo.db["runs_new"].find_one_or_404(
+            {key: value})
+        mongo.db["runs_new"].find_one_and_update(
+            {key: value},
+            {"$push": {"data": data}}
+        )
+        run_doc_after= mongo.db["runs_new"].find_one_or_404(
+            {key: value})
+        return flask.jsonify({"previous_run_doc": run_doc_before,
+                              "new_run_doc": run_doc_after})
+        
+    def _post_raw_data_(self, key, value, data):
+        pass
 
 
 class RunObjectIDData(RunData):
-    def get(self, object_id):
-        return self._get_("object_id", object_id)
+    def get(self, object_id, data_type=None):
+        if not data_type:
+            result = self._get_("object_id", object_id)
+            return flask.jsonify({results: result})
+        else:
+            pass
 
 
 class RunRunNumberData(RunData):
-    def get(self, run_number):
-        return self._get_("number", run_number)
-
+    def get(self, run_number, data_type=None):
+        if not data_type:
+            result = self._get_("number", run_number)
+            return flask.jsonify({results: result})
+        else:
+            pass
 
 class RunTimestampData(RunData):
-    def get(self, timestamp):
-        return self._get_("name", timestamp)
-
-
-class RunDataRaw(Resource):
-    def _put_(self, location):
-        if data_type:
-            query = {"data": {"$elemMatch": {"type": data_type,
-                                             "rse": location}}}
+    def get(self, timestamp, data_type=None):
+        print(timestamp)
+        if not data_type:
+            result = self._get_("name", timestamp)
+            return flask.jsonify({results: result})
         else:
-            query = {"data": {"$elemMatch": {"rse": location}}}
-        results = util.get_data_single_top_level(query, data_field)
-        app.logger.debug("results: %s" % results)
-        return results
+            pass
 
-
-
-api.add_resource(RunsLocationList,
-                 '/run/objectid/<ObjectId:object_id>/data/',
+api.add_resource(RunObjectIDData,
+                 '/run/objectid/<ObjectId:object_id>/data/'
+                 '/run/objectid/<ObjectId:object_id>/data/<string:data_type>/',
                  endpoint="run_object_id_data")
-api.add_resource(RunsLocationList,
+api.add_resource(RunObjectIDData,
                  '/runs/objectid/<ObjectId:object_id>/data/',
+                 '/runs/objectid/<ObjectId:object_id>/data/<string:data_type>/',
                  endpoint="runs_object_id_data")
 
 api.add_resource(RunRunNumberData,
-                 '/run/runnumber/<int:run_number>/data/',
+                 '/run/runnumber/<int:run_number>/data/<string:data_type>/',
                  endpoint="run_run_number_data")
 api.add_resource(RunRunNumberData,
-                 '/run/number/<int:run_number>/data/',
+                 '/run/number/<int:run_number>/data/<string:data_type>/',
                  endpoint="run_number_data")
 api.add_resource(RunRunNumberData,
-                 '/runs/runnumber/<int:run_number>/data/',
+                 '/runs/runnumber/<int:run_number>/data/<string:data_type>/',
                  endpoint="runs_run_number_data")
-api.add_resource(RunRunNumber,
-                 '/runs/number/<int:run_number>/data/',
+api.add_resource(RunRunNumberData,
+                 '/runs/number/<int:run_number>/data/<string:data_type>/',
                  endpoint="runs_number_data")
 
 api.add_resource(RunTimestampData,
-                 '/run/timestamp/<string:timestamp>/data/',
+                 '/run/timestamp/<string:timestamp>/data/<string:data_type>/',
                  endpoint="run_timestamp_data")
 api.add_resource(RunTimestampData,
-                 '/run/name/<string:timestamp>/data/',
+                 '/run/name/<string:timestamp>/data/<string:data_type>/',
                  endpoint="run_name_timestamp_data")
 api.add_resource(RunTimestampData,
-                 '/runs/timestamp/<string:timestamp>/data/',
+                 '/runs/timestamp/<string:timestamp>/data/<string:data_type>/',
                  endpoint="runs_timestamp_data")
 api.add_resource(RunTimestampData,
-                 '/runs/name/<string:timestamp>/data/',
+                 '/runs/name/<string:timestamp>/data/<string:data_type>/',
                  endpoint="runs_name_timestamp_data")
+
