543f362a8bed9f2002365f9354857a33267257ec
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-25 09:07:51 -0500

    Trying to add something to easily update location of data

---
+++ xenon_runsDB_api/run/location.py
@@ -0,0 +1,12 @@
+from flask_restful import Resource
+from xenon_runsDB_api.common import util
+from xenon_runsDB_api.app import app, api
+
+"""
+Get location of run data and update location of data
+
+TODO: Implement
+"""
+
+# add an rse
+# question about processed data location
