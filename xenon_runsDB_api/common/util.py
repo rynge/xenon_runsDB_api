cc451d09fe69535b488d7386ffb11be43accc54c
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-25 09:06:07 -0500

    Moving code around

--- xenon_runsDB_api/common/util.py
+++ xenon_runsDB_api/common/util.py
@@ -1,20 +1,36 @@
 import flask
-from .app import app, mongo
+from xenon_runsDB_api.app import app, mongo
 
 def get_data_single_top_level(query, additional_top_level=None):
+    """
+    Helper function for larger queries that will produce a list of output
+    
+    Args:
+        query (dict): Query passed to PyMongo
+        additional_top_level (str or list of str): Fields to add to returned
+                                                   query
+
+    Returns:
+        JSON with "results" key that has list of run docs with limited 
+        contents
+    """
+    # Reducing fields that will be returned by MongoDB
+    # Only return the Object ID, run number, and run name by default
+    # Add the desired fields either as a list of str
     top_level_fields = ["_id", "number", "name"]
     if isinstance(additional_top_level, list):
         top_level_fields = top_level_fields + additional_top_level
     elif isinstance(additional_top_level, str):
         top_level_fields.append(additional_top_level)
-    print(type(top_level_fields))
-    print(top_level_fields)
+    elif:
+        return flask.abort(404, "Please pass a string or list of fields")
     desired_fields = {tlf: 1 
                       for tlf in top_level_fields}
     cursor = mongo.db.runs_new.find(
         query,
         desired_fields)  
     app.logger.debug('Requesting %s records' % cursor.count())
+    # Need to convert cursor to list
     results = [x for x in cursor]
     app.logger.debug("results %s" % results)
     return flask.jsonify({"results": results})
