2a3f87891ef778e57e60888eca95649aa45780a8
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-08-26 12:40:02 -0500

    Checkpoint

--- xenon_runsDB_api/run/status.py
+++ xenon_runsDB_api/run/status.py
@@ -1,6 +1,19 @@
 from flask_restful import Resource
-from xenon_runsDB_api import util
+from xenon_runsDB_api.common import util
 from xenon_runsDB_api.app import app, api, mongo
+
+"""
+Translate run into a pseudo status.
+
+TODO: Implement
+"""
+
+
+class RunStatus(Resource):
+    def _put_(self, key, value, status):
+        pass
+        
+
 
 
 class RunStatusObjectID(Resource):
