60551fdcdb92c7d9031cf7d338de3cf5688dd7f8
Benedikt Riedel <benedikt.riedel@gmail.com>
2018-09-13 16:27:30 -0500

    first version of open ended queries

--- xenon_runsDB_api/runs/query.py
+++ xenon_runsDB_api/runs/query.py
@@ -1,55 +1,32 @@
-import ast
+import flask
 import flask_praetorian
 from flask_restful import Resource
-from flask import request
-from marshmallow import Schema
 from webargs import fields
 from webargs.flaskparser import use_args
-from xenon_runsDB_api.app import app, api, mongo
+from xenon_runsDB_api.app import app, api, mongo, config
 
-
-class runschema(Schema):
-    name = fields.Str()
-    number = fields.Int()
-    detector = fields.Str()
 
 user_args = {
-    'where': fields.Dict(keys=fields.Str())
+    'query': fields.Dict()
 }
 
 class RunsQuery(Resource):
+
     def __init__(self):
-        self.valid_query_requests = ["query", "q"]
-
-    # def reduce_args(self, args):
-    #     query = None
-    #     for k, v in args.items():
-    #         if k not in self.valid_query_requests:
-    #             continue
-    #         query = ast.literal_eval(v)
-    #     return query
-
-
-
+        self.mongodb = mongo.db[config["runsDB"]["database_name"]]
 
     @use_args(user_args)
-    def get(self):
+    def post(self, args, locations=["json"]):
         # args = request.args
         app.logger.debug("args passed to query: %s"
                          % args)
-        app.logger.debug("%s" % request.form["data"])
-        query = self.reduce_args(args)
-        cursor = mongo.db.runs_new.find(query)
+        query = args["query"]
+        cursor = self.mongodb.find(query)
         results = [x for x in cursor]
-        if len(results) == 1:
-            return results[0]
-        else:
-            return results
+        app.logger.debug("results: %s" % results)
+        return flask.jsonify({"results": results})
 
 
-api.add_resource(RunsQuery,
-                 '/run/query',
-                 endpoint="run_query")
 api.add_resource(RunsQuery,
                  '/runs/query',
                  endpoint="runs_query")
